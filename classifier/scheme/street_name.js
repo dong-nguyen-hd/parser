22062c212e6482035a7aa55c3fc0ce2c32c60564
DongND <dong.nguyen.hdkt@gmail.com>
2022-10-28 14:57:49 +0700

    update

--- classifier/scheme/street_name.js
+++ classifier/scheme/street_name.js
@@ -1,72 +1,156 @@
 const StreetNameClassification = require('../../classification/StreetNameClassification')
 
 module.exports = [
-  // {
-  //   // dos Fiéis
-  //   confidence: 0.5,
-  //   Class: StreetNameClassification,
-  //   scheme: [
-  //     {
-  //       is: ['StopWordClassification'],
-  //       not: ['DirectionalClassification', 'IntersectionClassification']
-  //     },
-  //     {
-  //       is: ['AlphaClassification', 'PersonClassification'],
-  //       not: ['StreetClassification', 'IntersectionClassification', 'StreetSuffixClassification']
-  //     }
-  //   ]
-  // },
-  // {
-  //   // Academia das Ciências
-  //   confidence: 0.5,
-  //   Class: StreetNameClassification,
-  //   scheme: [
-  //     {
-  //       is: ['AlphaClassification'],
-  //       not: ['StreetClassification', 'IntersectionClassification', 'StopWordClassification', 'StreetPrefixClassification']
-  //     },
-  //     {
-  //       is: ['StopWordClassification'],
-  //       not: ['DirectionalClassification']
-  //     },
-  //     {
-  //       is: ['AlphaClassification', 'PersonClassification'],
-  //       not: ['StreetClassification', 'IntersectionClassification', 'StreetSuffixClassification']
-  //     }
-  //   ]
-  // },
-  // {
-  //   // du 4 septembre
-  //   confidence: 0.5,
-  //   Class: StreetNameClassification,
-  //   scheme: [
-  //     {
-  //       is: ['StopWordClassification'],
-  //       not: ['IntersectionClassification']
-  //     },
-  //     {
-  //       is: ['NumericClassification'],
-  //       not: ['PostcodeClassification']
-  //     },
-  //     {
-  //       is: ['AlphaClassification'],
-  //       not: ['StreetClassification', 'IntersectionClassification', 'DistrictClassification']
-  //     }
-  //   ]
-  // },
-  // {
-  //   // dos Fiéis de Deus
-  //   confidence: 0.5,
-  //   Class: StreetNameClassification,
-  //   scheme: [
-  //     {
-  //       is: ['StreetNameClassification'],
-  //       not: ['StreetClassification', 'IntersectionClassification']
-  //     },
-  //     {
-  //       is: ['StreetNameClassification'],
-  //       not: ['StreetClassification', 'IntersectionClassification']
-  //     }
-  //   ]
-  // }
+  {
+    //  Lê Văn Hiến
+    confidence: 0.5,
+    Class: StreetNameClassification,
+    scheme: [
+      {
+        is: ['SurnameClassification'],
+        not: []
+      },
+      {
+        is: ['AlphaClassification'],
+        not: []
+      },
+      {
+        is: ['AlphaClassification'],
+        not: []
+      }
+    ]
+  },
+  {
+    //  Nguyễn Đông
+    confidence: 0.5,
+    Class: StreetNameClassification,
+    scheme: [
+      {
+        is: ['SurnameClassification'],
+        not: []
+      },
+      {
+        is: ['AlphaClassification'],
+        not: []
+      }
+    ]
+  },
+  {
+    //  Nguyễn Đông Anh Phan
+    confidence: 0.5,
+    Class: StreetNameClassification,
+    scheme: [
+      {
+        is: ['SurnameClassification'],
+        not: []
+      },
+      {
+        is: ['AlphaClassification'],
+        not: []
+      },
+      {
+        is: ['AlphaClassification'],
+        not: []
+      },
+      {
+        is: ['AlphaClassification'],
+        not: []
+      }
+    ]
+  },
+  {
+    //  đường Cách mạng tháng Tám
+    confidence: 0.98,
+    Class: StreetNameClassification,
+    scheme: [
+      {
+        is: ['StreetPrefixClassification', 'RoadTypeClassification', 'SurnameClassification'],
+        not: ['CommuneClassification']
+      },
+      {
+        is: ['AlphaClassification'],
+        not: []
+      },
+      {
+        is: ['AlphaClassification'],
+        not: []
+      },
+      {
+        is: ['AlphaClassification'],
+        not: []
+      },
+      {
+        is: ['AlphaClassification'],
+        not: []
+      }
+    ]
+  },
+  {
+    // dos Fiéis
+    confidence: 0.5,
+    Class: StreetNameClassification,
+    scheme: [
+      {
+        is: ['StopWordClassification'],
+        not: ['DirectionalClassification', 'IntersectionClassification']
+      },
+      {
+        is: ['AlphaClassification', 'PersonClassification'],
+        not: ['StreetClassification', 'IntersectionClassification', 'StreetSuffixClassification']
+      }
+    ]
+  },
+  {
+    // Academia das Ciências
+    confidence: 0.5,
+    Class: StreetNameClassification,
+    scheme: [
+      {
+        is: ['AlphaClassification'],
+        not: ['StreetClassification', 'IntersectionClassification', 'StopWordClassification', 'StreetPrefixClassification']
+      },
+      {
+        is: ['StopWordClassification'],
+        not: ['DirectionalClassification']
+      },
+      {
+        is: ['AlphaClassification', 'PersonClassification'],
+        not: ['StreetClassification', 'IntersectionClassification', 'StreetSuffixClassification']
+      }
+    ]
+  },
+  {
+    // du 4 septembre
+    confidence: 0.5,
+    Class: StreetNameClassification,
+    scheme: [
+      {
+        is: ['StopWordClassification'],
+        not: ['IntersectionClassification']
+      },
+      {
+        is: ['NumericClassification'],
+        not: ['PostcodeClassification']
+      },
+      {
+        is: ['AlphaClassification'],
+        not: ['StreetClassification', 'IntersectionClassification', 'DistrictClassification']
+      }
+    ]
+  },
+  {
+    // dos Fiéis de Deus
+    confidence: 0.5,
+    Class: StreetNameClassification,
+    scheme: [
+      {
+        is: ['StreetNameClassification'],
+        not: ['StreetClassification', 'IntersectionClassification']
+      },
+      {
+        is: ['StreetNameClassification'],
+        not: ['StreetClassification', 'IntersectionClassification']
+      }
+    ]
+  }
 ]
