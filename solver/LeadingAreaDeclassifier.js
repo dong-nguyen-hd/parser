04b3db35d2f32886c2c326c200e88c0cd7239eb8
DongND <dong.nguyen.hdkt@gmail.com>
2023-01-13 14:49:32 +0700

    update 1.13

--- solver/LeadingAreaDeclassifier.js
+++ solver/LeadingAreaDeclassifier.js
@@ -14,27 +14,40 @@ const ADMIN_CLASSIFICATIONS = [
 ]
 
 class LeadingAreaDeclassifier {
-  solve (tokenizer) {
-    // tokenizer.solution.forEach(s => {
-    //   // record the position of the last non-admin cursor position
-    //   let lastNonAdminCursorPosition = 0
+  solve(tokenizer) {
+    tokenizer.solution = tokenizer.solution.filter(s => {
+      // // record the position of the last non-admin cursor position
+      // let lastNonAdminCursorPosition = 0
 
-    //   for (let i = 0; i < s.pair.length; i++) {
-    //     const isAdmin = ADMIN_CLASSIFICATIONS.some(c => s.pair[i].classification.constructor.name === c)
-    //     const isNeut = NETURAL_CLASSIFICATIONS.some(c => s.pair[i].classification.constructor.name === c)
-    //     if (!isAdmin && !isNeut) {
-    //       lastNonAdminCursorPosition = s.pair[i].span.end
-    //     }
-    //   }
+      // for (let i = 0; i < s.pair.length; i++) {
+      //   const isAdmin = ADMIN_CLASSIFICATIONS.some(c => s.pair[i].classification.constructor.name === c)
+      //   if (!isAdmin) {
+      //     lastNonAdminCursorPosition = s.pair[i].span.end
+      //   }
+      // }
 
-    //   s.pair = s.pair.filter(p => {
-    //     const isAdmin = ADMIN_CLASSIFICATIONS.some(c => p.classification.constructor.name === c)
-    //     if (isAdmin && p.span.end < lastNonAdminCursorPosition) {
-    //       return false
-    //     }
-    //     return true
-    //   })
-    // })
+      // s.pair = s.pair.filter(p => {
+      //   const isAdmin = ADMIN_CLASSIFICATIONS.some(c => p.classification.constructor.name === c)
+      //   if (isAdmin && p.span.end < lastNonAdminCursorPosition) {
+      //     return false
+      //   }
+      //   return true
+      // })
+
+      // Filter invalid solutions
+      // Case: region abbreviation
+      var area = {};
+      s.pair.forEach(p => {
+        if (p.classification.constructor.name === ADMIN_CLASSIFICATIONS[2]) area.region = p;
+        if (p.classification.constructor.name === ADMIN_CLASSIFICATIONS[1]) area.county = p;
+        if (p.classification.constructor.name === ADMIN_CLASSIFICATIONS[0]) area.locality = p;
+      })
+
+      var keys = Object.keys(area);
+      if (!!area.region && keys.length > 1 && area.region.span.start == 0 && area.region.span.body.length < 10) return false
+      
+      return true
+    })
 
     tokenizer.solution.sort((a, b) => b.score - a.score) // sort results by score desc
     tokenizer.solution.forEach(s => s.pair.sort((a, b) => a.span.start - b.span.start)) // sort by span start
